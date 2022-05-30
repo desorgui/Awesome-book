744ada301c85f6c29557a6403d04ac498ff4c526
Desor Guishny <desorguishny@gmail.com>
2022-05-30 18:28:17 -0400

    Add event to dynamic content

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
 let bookName = document.getElementById('bookName');
-bookName = '';
 let authorName = document.getElementById('authorName');
-authorName = '';
 let newBook = bookName + authorName ;
 let addBook = document.getElementById('addBook');
 
-let book = [
-    {nameOfBook: bookName,
-     nameOfAuthor: authorName}
-];
+let book = [];
 let books = document.getElementById('books')
 
 addBook.addEventListener('click', () => {
+    book.nameOfBook = bookName.value;
+    book.nameOfAuthor = authorName.value;
  const bookItem = document.createElement('div');
  bookItem.classList.add('bookItem');
- console.log("clicked");
+ console.log(book);
  bookItem.innerHTML = `
  <div class="book-content">
-    <p>${book.bookName}</p>
-    <p>${book.authorName}</p>
+    <p>${book.nameOfBook}</p>
+    <p>${book.nameOfAuthor}</p>
     <div class="remove-book">
-        <button type="button" id="remove">Remove</button>
+        <button type="button" class="remove">Remove</button>
     </div>
 </div>
 `;
  books.appendChild(bookItem);
+ let removeBook = bookItem.querySelectorAll('.remove');
+
+ removeBook.forEach(element => {
+     element.addEventListener('click', () => {
+        bookItem.remove();
+     })
+ });
 })
