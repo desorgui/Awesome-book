8e13c8e34046f014a4aae9bfa16b1433bfdfb40a
Maury PC <tryptic-st@outlook.com>
2022-05-30 16:12:41 -0500

    Add function and style to the book created

--- main.js
+++ main.js
@@ -5,11 +5,24 @@ authorName = '';
 let newBook = bookName + authorName ;
 let addBook = document.getElementById('addBook');
 
-let book = [];
+let book = [
+    {nameOfBook: bookName,
+     nameOfAuthor: authorName}
+];
 let books = document.getElementById('books')
 
 addBook.addEventListener('click', () => {
  const bookItem = document.createElement('div');
  bookItem.classList.add('bookItem');
+ console.log("clicked");
+ bookItem.innerHTML = `
+ <div class="book-content">
+    <p>${book.bookName}</p>
+    <p>${book.authorName}</p>
+    <div class="remove-book">
+        <button type="button" id="remove">Remove</button>
+    </div>
+</div>
+`;
  books.appendChild(bookItem);
 })
