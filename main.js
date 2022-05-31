4071e895b6407dfc345fbebda2ef9d3023dce980
Desor Guishny <desorguishny@gmail.com>
2022-05-31 10:45:19 -0400

    Clear linters

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
-let bookName = document.getElementById('bookName');
-let authorName = document.getElementById('authorName');
-let newBook = bookName + authorName ;
-let addBook = document.getElementById('addBook');
+const bookName = document.getElementById('bookName');
+const authorName = document.getElementById('authorName');
+const newBook = bookName + authorName;
+const addBook = document.getElementById('addBook');
 
-let book = [];
-let books = document.getElementById('books')
+const book = [];
+const books = document.getElementById('books');
 
 addBook.addEventListener('click', () => {
-    book.nameOfBook = bookName.value;
-    book.nameOfAuthor = authorName.value;
- const bookItem = document.createElement('div');
- bookItem.classList.add('bookItem');
- console.log(book);
- bookItem.innerHTML = `
- <div class="book-content">
+  book.nameOfBook = bookName.value;
+  book.nameOfAuthor = authorName.value;
+  const bookItem = document.createElement('div');
+  bookItem.classList.add('bookItem');
+  bookItem.innerHTML = `
+  <div class="book-content">
     <p>${book.nameOfBook}</p>
     <p>${book.nameOfAuthor}</p>
     <div class="remove-book">
@@ -21,12 +20,12 @@ addBook.addEventListener('click', () => {
     </div>
 </div>
 `;
- books.appendChild(bookItem);
- let removeBook = bookItem.querySelectorAll('.remove');
+  books.appendChild(bookItem);
+  const removeBook = bookItem.querySelectorAll('.remove');
 
- removeBook.forEach(element => {
-     element.addEventListener('click', () => {
-        bookItem.remove();
-     })
- });
-})
+  removeBook.forEach(element => {
+    element.addEventListener('click', () => {
+      bookItem.remove();
+    });
+  });
+});
