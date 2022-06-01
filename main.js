2fb87a75514e290c87f315189ab6cd9ac584d4e9
Maury PC <tryptic-st@outlook.com>
2022-06-01 10:18:12 -0500

    linters check

--- main.js
+++ main.js
@@ -2,16 +2,15 @@ const bookName = document.getElementById('bookName');
 const authorName = document.getElementById('authorName');
 const addBook = document.getElementById('addBook');
 
-let booksContainer = localStorage.getItem("books") === null || localStorage.getItem('books') === undefined ? [] : JSON.parse(localStorage.getItem('books'));
-var book = [];
+let booksContainer = localStorage.getItem('books') === null || localStorage.getItem('books') === undefined ? [] : JSON.parse(localStorage.getItem('books'));
+let book = [];
 const books = document.getElementById('books');
 
-
 const bookItem = document.createElement('div');
-if(booksContainer.length > 0){
-booksContainer.forEach((element) => {
-  bookItem.classList.add('bookItem');
-  bookItem.innerHTML += `
+if (booksContainer.length > 0) {
+  booksContainer.forEach((element) => {
+    bookItem.classList.add('bookItem');
+    bookItem.innerHTML += `
     <div class="book-content">
       <p>${element[0]} by ${element[1]}</p>
       <div class="remove-book">
@@ -20,28 +19,24 @@ booksContainer.forEach((element) => {
     </div>
   `;
   books.appendChild(bookItem);
-});
-}
+  });
+  }
 else booksContainer = [];
 const removeBook = bookItem.querySelectorAll('.remove-book');
 
-  removeBook.forEach((element, index) => {
-    element.addEventListener('click', () => {      
-      booksContainer.splice(index, 1);
-      console.log(booksContainer);
-      console.log(localStorage);
-      localStorage.setItem("books", JSON.stringify(booksContainer));
-      location.reload();
-    });
-    
+removeBook.forEach((element, index) => {
+  element.addEventListener('click', () => {      
+    booksContainer.splice(index, 1);
+    localStorage.setItem("books", JSON.stringify(booksContainer));
+    location.reload();
   });
+});
 
 addBook.addEventListener('click', () => {
   if (bookName.value && authorName.value){
   book = [bookName.value, authorName.value];  
   booksContainer.push(book);
   localStorage.setItem("books", JSON.stringify(booksContainer));
-  console.log(booksContainer);
   location.reload();
 }
 /*
