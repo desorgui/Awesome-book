2e07a7fc06cc35da8cec0d8f4e7502ec1a0e115c
Maury PC <tryptic-st@outlook.com>
2022-06-02 14:29:15 -0500

    Add cursor to the links and the display-flex class

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ if (booksContainer.length > 0) {
   booksContainer.forEach((element) => {
     bookItem.classList.add('bookItem');
     bookItem.innerHTML += `
-    <div class="book-content">
+    <div class="book-content d-flex">
       <p>${element[0]} by ${element[1]}</p>
       <div class="remove-book">
           <button type="button" class="remove">Remove</button>
